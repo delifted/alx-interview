7298c75f67c65369a5a8faa1367a7e435b72ba2f
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2024-04-30 07:59:44 +0100

    Solving Pascal's Triangle using python

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,11 +8,16 @@ def pascal_triangle(n: int) -> list[list[int]]:
     '''
     Function for the triangle
     '''
-    res = [[1]]
-    for i in range(n - 1):
-        temp = [0] + res[-1] + [0]
-        row = []
-        for j in range(len(res[-1]) + 1):
-            row.append(temp[j] + temp[j + 1])
-        res.append(row)
-    return res
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [[1]]
+    else:
+        res = [[1]]
+        for i in range(n - 1):
+            temp = [0] + res[-1] + [0]
+            row = []
+            for j in range(len(res[-1]) + 1):
+                row.append(temp[j] + temp[j + 1])
+            res.append(row)
+        return res
