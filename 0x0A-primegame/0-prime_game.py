59c411a5bdccdc0179873ab07e92065cb2c00b64
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2024-07-06 07:35:23 +0100

    message

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''
+Prime numbers
+'''
+
+
+def isWinner(x, nums):
+    '''
+    Determine the winner of each game based on the given rules.
+
+    Args:
+        x (int): The number of rounds.
+        nums (list): An array of n, where n may be different for each rounds
+
+    Returns:
+        str or None: The name o the player that won the most rounds.
+                    If the winner cannot be determined, returns None.
+    '''
+    if x < 1 or not nums:
+        return None
+    marias_wins, bens_wins = 0, 0
+    # generate primes with a limit of the maximum number in nums
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    # filter the number of primes less than n in nums for each round
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+        if marias_wins == bens_wins:
+            return None
+        return 'Maria' if marias_wins > bens_wins else 'Ben'
